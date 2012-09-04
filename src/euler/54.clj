0cd4dbccc093247c391862c20119ce94673141d2
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2012-09-03 20:26:18 -0500

    refactor

--- src/euler/54.clj
+++ src/euler/54.clj
@@ -1,46 +1,7 @@
 (ns euler.54)
 
-(declare has-straight-flush has-flush has-straight has-two-pair who-wins has-highest-card has-pair pair-winner pair-card has-triple triple-card has-full-house has-quadruple quadruple-card)
+(declare handz get-highest-satisfied-category has-straight-flush has-flush has-straight has-two-pair who-wins has-highest-card has-pair pair-winner pair-card has-triple triple-card has-full-house has-quadruple quadruple-card)
 
-(def handz
-  [{:hand :straight-flush
-    :priority 9
-    :detector has-straight-flush}
-   {:hand :quadruple
-    :priority 8
-    :detector has-quadruple}
-   {:hand :full-house
-    :priority 7
-    :detector has-full-house}
-   {:hand :flush
-    :priority 6
-    :detector has-flush}
-   {:hand :straight
-    :priority 5
-    :detector has-straight}
-   {:hand :triple
-    :priority 4
-    :detector has-triple}
-   {:hand :two-pair
-    :priority 3
-    :detector has-straight}
-   {:hand :pair
-    :priority 2
-    :detector has-pair}
-   {:hand :straight
-    :priority 1
-    :detector (fn [] true)}])
-
-(def category-hierarchy
-  {:highest-card 1
-   :pair 2
-   :two-pair 3
-   :triple 4
-   :straight 5 
-   :flush 6
-   :full-house 7
-   :quadruple 8
-   :straight-flush 9})
 
 (defn hand-summary [hand composing-card]
   {:hand hand :composing-card composing-card})
@@ -48,45 +9,48 @@
 (defn sort-descending [hand]
   (sort #(> %1 %2) hand))
 
-(defn get-highest-hand [hand]
-  (let [hand-numbers (map :number hand)]
-  (cond
-    (has-straight-flush hand hand-numbers) (hand-summary :straight-flush (apply max hand-numbers))
-    (has-quadruple hand-numbers) (hand-summary :quadruple (quadruple-card hand-numbers))
-    (has-full-house hand-numbers) (hand-summary :full-house (apply max hand-numbers))
-    (has-flush hand) (hand-summary :flush (apply max hand-numbers))
-    (has-straight hand-numbers) (hand-summary :straight (apply max hand-numbers))
-    (has-triple hand-numbers) (hand-summary :triple (triple-card hand-numbers))
-    (has-two-pair hand-numbers) (hand-summary :two-pair (pair-card hand-numbers))
-    (has-pair hand-numbers) (hand-summary :pair (pair-card hand-numbers))
-    :else (hand-summary :highest-card (apply max hand-numbers)))))
+(defn build-hand [hand hand-numbers]
+  (let [composing-card-fn (:composing-card hand)]
+    (assoc 
+      hand 
+      :composing-card 
+      (composing-card-fn hand-numbers))))
+
+(defn satisfied [configuration hand hand-numbers]
+ (let [detector (:detector configuration)]
+   (detector hand hand-numbers)))
+
+
+(defn get-highest-satisfied-category [hand]
+  (let [hand-numbers (map :number hand)
+        hands-satisfied (filter #(satisfied % hand hand-numbers) handz)
+        first-satisfied (first hands-satisfied)
+        ]
+    (build-hand
+      first-satisfied
+      hand-numbers)))
 
 (defn analyze-hand [hand]
   {:sorted-cards (sort-descending (map :number hand))
-   :highest-hand (get-highest-hand hand)})
-
-(defn category-beats [first-category second-category]
-  (> (first-category category-hierarchy)
-     (second-category category-hierarchy)))
+   :highest-satisfied-category (get-highest-satisfied-category hand)})
 
 (defn category-winner [first-hand second-hand]
-  (who-wins #(category-beats
-               (:hand (:highest-hand %1))
-               (:hand (:highest-hand %2)))
+  (who-wins #(>
+               (:priority (:highest-satisfied-category %1))
+               (:priority (:highest-satisfied-category %2)))
             first-hand
             second-hand))
 
 (defn composing-card-winner [first-hand second-hand]
   (who-wins #(>
-               (:composing-card (:highest-hand %1))
-               (:composing-card (:highest-hand %2)))
+               (:composing-card (:highest-satisfied-category %1))
+               (:composing-card (:highest-satisfied-category %2)))
             first-hand
             second-hand))
 
 (defn hand-based-winner [first-hand second-hand]
   (or (category-winner first-hand second-hand)
     (composing-card-winner first-hand second-hand)))
-
 
 (defn winner [first-hand second-hand]
   (let [first-hand-analyzed (analyze-hand first-hand)
@@ -138,32 +102,32 @@
     (safe-max (filter #(= 4 (get occurrences %))
                        hand))))
 
-(defn has-pair [hand]
-  (not (nil? (pair-card hand))))
+(defn has-pair [_ hand-numbers]
+  (not (nil? (pair-card hand-numbers))))
 
-(defn has-two-pair [hand]
-  (let [occurrences (occurrences-hash hand) ]
+(defn has-two-pair [_ hand-numbers]
+  (let [occurrences (occurrences-hash hand-numbers) ]
     (= 2 (count (set (filter #(= 2 (get occurrences %))
-                         hand))))))
+                         hand-numbers))))))
 
-(defn has-triple [hand]
-  (not (nil? (triple-card hand))))
+(defn has-triple [_ hand-numbers]
+  (not (nil? (triple-card hand-numbers))))
 
-(defn has-quadruple [hand]
-  (not (nil? (quadruple-card hand))))
+(defn has-quadruple [_ hand-numbers]
+  (not (nil? (quadruple-card hand-numbers))))
 
-(defn has-straight [hand]
-  (= (sort hand) (range (apply min hand) (inc (apply max hand)))))
+(defn has-straight [_ hand-numbers]
+  (= (sort hand-numbers) (range (apply min hand-numbers) (inc (apply max hand-numbers)))))
 
-(defn has-flush [hand]
+(defn has-flush [hand _]
   (= 1 (count (set (map :suit hand)))))
 
 (defn has-straight-flush [hand hand-numbers]
-  (and (has-flush hand)
-       (has-straight hand-numbers)))
+  (and (has-flush hand hand-numbers)
+       (has-straight hand hand-numbers)))
 
-(defn has-full-house [hand]
-  (let [occurrences (occurrences-hash hand)]
+(defn has-full-house [_ hand-numbers]
+  (let [occurrences (occurrences-hash hand-numbers)]
     (and (= 3 (apply max (vals occurrences)))
          (= 2 (apply min (vals occurrences))))))
 
@@ -175,3 +139,40 @@
       (who-wins #(> (first %1) (first %2))
                 first-hand
                 second-hand))))
+(def handz
+  [{:hand :straight-flush
+    :priority 9
+    :detector has-straight-flush
+    :composing-card #(apply max %)}
+   {:hand :quadruple
+    :priority 8
+    :detector has-quadruple
+    :composing-card #(quadruple-card %)}
+   {:hand :full-house
+    :priority 7
+    :detector has-full-house
+    :composing-card #(apply max %)}
+   {:hand :flush
+    :priority 6
+    :detector has-flush
+    :composing-card #(apply max %)}
+   {:hand :straight
+    :priority 5
+    :detector has-straight
+    :composing-card #(apply max %)}
+   {:hand :triple
+    :priority 4
+    :detector has-triple
+    :composing-card #(triple-card %)}
+   {:hand :two-pair
+    :priority 3
+    :detector has-two-pair
+    :composing-card #(pair-card %)}
+   {:hand :pair
+    :priority 2
+    :detector has-pair
+    :composing-card #(pair-card %)}
+   {:hand :highest-card
+    :priority 1
+    :detector (fn [_ _] true)
+    :composing-card #(apply max %)}])
