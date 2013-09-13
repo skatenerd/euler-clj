710d371ed5484506579019c13eff5d4f8d1ba24b
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2013-09-12 19:51:44 -0500

    Finish 59

--- src/euler/59.clj
+++ src/euler/59.clj
@@ -7,12 +7,11 @@
       (slurp "src/euler/word_list.txt")
       #"\r\n")))
 
-(def cipher
+(def cipher-file
   (map #(-> % clojure.string/trim Integer/parseInt)
        (clojure.string/split
-       (slurp "src/euler/cipher59.txt")
-       #",")))
-
+         (slurp "src/euler/cipher59.txt")
+         #",")))
 
 (defn unbitify [s]
   (let [pows-of-2 (keep-indexed #(if (= 1 %2) %1) (reverse s))]
@@ -39,17 +38,30 @@
 (defn bits-for-char [c]
   (bitify (int c)))
 
-(defn xor [a b] (if (= a b) 0 1))
+(defn xor [a b]
+  (if (= a b) 0 1))
 
 (defn cipher [text cipher-key]
-  (let [cipher-bits (mapcat bits-for-char cipher-key)
-        repeated-cipher (mapcat identity (repeat cipher-bits))
-        text-bits (mapcat bits-for-char text)
-        ciphered-bits (map xor text-bits repeated-cipher)]
-    (prn ciphered-bits)
-    (map char-for-bits (partition 8 ciphered-bits))
+ (let [cipher-bits (map bits-for-char cipher-key)
+       repeated-cipher (mapcat identity (repeat cipher-bits))
+       text-bits (map bits-for-char text)
+       ciphered-bits (map #(map xor %1 %2) text-bits repeated-cipher)]
+    (map char-for-bits ciphered-bits)))
 
-    )
+(defn score-raw [chars]
+  (count
+    (filter
+      #(pos? (java.util.Collections/indexOfSubList (apply list chars) (apply list (str " " % " "))))
+      word-set)))
 
+(defn score-ciphered [to-score cipher-key]
+  (score-raw (cipher to-score cipher-key)))
 
-  )
+(defn interesting-combos []
+  (for [first-cipher (range (int \a) (inc (int \z)))
+        second-cipher (range (int \a) (inc (int \z)))
+        third-cipher (range (int \a) (inc (int \z)))]
+    (let [the-cipher (map char [first-cipher second-cipher third-cipher])
+          the-score (score-ciphered cipher-file the-cipher)]
+      (if (< 8 the-score)
+        (prn the-cipher the-score)))))
--- test/euler/59_test.clj
+++ test/euler/59_test.clj
@@ -15,5 +15,20 @@
   (testing "cipher a sequence of chars"
         (let [cipher-key [\A \A \A]
               to-cipher [\k \k \k \k \k \k]]
-          (is (= \* (first (cipher to-cipher cipher-key))))))
-         )
+          (is (= \* (first (cipher to-cipher cipher-key))))
+          (is (= to-cipher (cipher (cipher to-cipher cipher-key) cipher-key))))))
+
+(deftest score
+  (testing "high score"
+    (is (= 2 (score-raw (seq "zzz they zzz with zzzzzzzzzzzz")))))
+
+  (testing "cipher score"
+     (is (= 2 (score-ciphered
+                (cipher "zzz they zzz with zzzzzzzzzzzz" [\X \Y \X])
+                [\X \Y \X])))))
+
+(deftest answer
+   (let [original-text  (cipher cipher-file [\g \o \d])
+         bits-for-text (map bits-for-char original-text)
+         nums-for-text (map unbitify bits-for-text)]
+     (prn (apply + nums-for-text))))
